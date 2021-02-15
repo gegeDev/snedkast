5ce429c357d065bb3a41d6a3f9eece4756136342
Grzegorz Płaczek <gegepl123@gmail.com>
2021-02-15 11:13:58 +0100

    Calculating momentary positions

--- computing.cpp
+++ computing.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 struct Settings
 {
-    int g;
-    int hd;
+    float g;
+    float hd;
+    float step;
+    float velocityY;
+    float velocityX;
 };
 int main()
 {
     Settings params;
-    cin >> params.g >> params.hd;
-    cout << params.g << " " << params.hd << endl;
+    cin >> params.g >> params.hd >> params.step >> params.velocityY >> params.velocityX;
+    float time = 2.0 * params.velocityY / params.g;
+    cout << time << endl;
+    float timeDef = time / params.step;
+    for(float i = 0; i <= time; i+= timeDef)
+    {
+        float xPlace = params.velocityX * i;
+        float yPlace = params.velocityY * i - (params.g * i * i / 2);
+        cout << i << " " << xPlace << yPlace << endl; 
+    }
     return 0;
 }
